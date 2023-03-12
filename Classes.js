2d5e9e4253eeeeb6cd311e488da2a974f48632ba
Khaled Alghanim <al.ghanim@me.com>
2023-03-12 16:05:08 +0100

    Done

--- Classes.js
+++ Classes.js
@@ -21,7 +21,27 @@
  */
 class Person {
   // continue the code here
+
+  constructor(firstName, lastName, gender, birthYear){
+    this.firstName = firstName
+    this.lastName = lastName
+    this.gender = gender
+    this.birthYear = birthYear
+  }
+
+  printName() {
+    console.log(`${this.firstName} ${this.lastName}`)
+  }
+
+  calculateAge() {
+    let currentYear = new Date().getFullYear()
+    console.log(currentYear - this.birthYear)
+  }
 }
+
+let khaled = new Person('Khaled', 'Alghanim', 'Male', 1986)
+khaled.printName()
+khaled.calculateAge()
 
 /** (Question 2): (15000 Points)
  * 1. Write a class `Movie`, give it the following properties
@@ -46,7 +66,38 @@ class Person {
 
 class Movie {
   // continue the code here
+  constructor(title, duration, genre) {
+    this.title = title
+    this.duration = duration
+    this.genre = genre
+    this.rating = []
+  }
+
+  rate(rating) {
+    if (rating >= 0 && rating <=10) {
+      this.rating.push(rating)
+    } else {
+      console.log('Rating should be between 0 and 10')
+    }
+  }
+
+  averageRating() {
+    const sum = this.rating.reduce(
+      (accumulator, currentValue) => accumulator + currentValue, 0
+    );
+    const avg = (sum / this.rating.length)
+    
+    return avg
+  }
 }
+
+let batman = new Movie('Batman', 60, 'Action')
+batman.rate(10)
+batman.rate(7.5)
+batman.rate(9.5)
+
+console.log(`Ratings are ${batman.rating}`)
+console.log(`${batman.title} movie average ratings is ${batman.averageRating()}`)
 
 /** (Question 3): (1000 Points)
  * 1. Create a class `Actor` that inherits `Person`, and adds the following properties
@@ -58,3 +109,19 @@ class Movie {
  */
 
 // write the class here
+
+class Actor extends Person {
+  constructor(firstName, lastName, gender, birthYear) {
+    super(firstName, lastName, gender, birthYear)
+    this.movies = []
+  }
+
+  addMovie(movie) {
+    this.movies.push(movie)
+  }
+}
+
+let tareq = new Actor('Tareq', 'Al-Ali', 'Male', 1965)
+tareq.addMovie(batman)
+tareq.printName()
+console.log(tareq.movies)
